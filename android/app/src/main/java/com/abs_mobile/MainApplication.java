d558543505fc5e2a4c233f64294f389e641fef4c
mahmud <mahmud120398@gmail.com>
2020-11-17 15:57:52 +0700

    change deault name of apps

--- android/app/src/main/java/com/abs_mobile/MainApplication.java
+++ android/app/src/main/java/com/abs_mobile/MainApplication.java
@@ -63,7 +63,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.satu_data.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.abs_mobile.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
